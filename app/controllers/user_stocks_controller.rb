78ae1195340f841e8b9bfb9378b0cb7ed9adb4bb
Exellin <carnegieshawn@yahoo.ca>
2016-04-11 01:32:17 +0000

    fixed bug with deleting relations

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -76,6 +76,7 @@ class UserStocksController < ApplicationController
   # DELETE /user_stocks/1
   # DELETE /user_stocks/1.json
   def destroy
+    debugger
     @user_stock.destroy
     respond_to do |format|
       format.html { redirect_to my_portfolio_path, notice: 'Stock was successfully removed from portfolio' }
@@ -86,7 +87,7 @@ class UserStocksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user_stock
-      @user_stock = UserStock.where(stock_id: params[:id]).first
+      @user_stock = UserStock.where(stock_id: params[:id]).where(user_id: current_user.id).first
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
